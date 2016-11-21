3b0c62700e2819f8ace7a946bad0691e3d31f5b9
musci8 <federico.musciotto@unipa.it>
2016-11-21 10:40:11 +0100

    Update overExpression.py

--- overExpression.py
+++ overExpression.py
@@ -1,4 +1,7 @@
 import scipy.stats as st
+import numpy as np
+from multiprocessing import Pool
+import igraph 
 
 def oeHypergeom(N,N1,N2,N12,mode=['over','under','both']):
     if mode=='over':
@@ -13,3 +16,18 @@ def oeHypergeom(N,N1,N2,N12,mode=['over','under','both']):
       return [p1,p2]
     else:
       raise Exception('Mode is not valid')
+    
+def SVN(mat,tres):
+    N = mat.shape[0]
+    A,B = np.triu_indices(N,k=1)
+    X = zip(A,B)
+    A = mat[X]
+    p = Pool(processes=16)
+    D = zip(X, p.map(pvalues, A,16))
+    D = sorted(D,key=lambda x : x[1])
+    P = np.array(zip(*D)[1])
+    K = 2*tres/(N*(N-1)) * np.arange(1,len(P)+1)
+    i = np.where(P<K)[0]
+    edges = list(np.array(zip(*D)[0])[i])
+    g = igraph.Graph(N,edges)
+    return g,edges
